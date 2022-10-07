77417308082cd89a03d722803f5a04295b832f95
Maxime Coriton <maxime.coriton@gmail.com>
2022-10-07 13:47:44 +0200

    📦 NEW: add nuxt image optimization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   
-  modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss",'@nuxt/image-edge'],
   css: ["~/assets/css/main.css"],
   build: {
     postcss: {
