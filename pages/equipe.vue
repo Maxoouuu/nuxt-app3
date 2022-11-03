ae274f83e9b4f8f76aae5e64bff0a312355e5e27
Maxime Coriton <maxime.coriton@gmail.com>
2022-11-03 12:33:55 +0100

    equipe

--- pages/equipe.vue
+++ pages/equipe.vue
@@ -1,5 +1,47 @@
 <template>
     <div>
-        
+        <div class="flex justify-center text-center text-3xl pb-6">Equipe</div>
+        <div class="max-w-4xl grid grid-cols-4 gap-4 mx-auto">
+
+            <div v-for="member in team" :key="member.id">
+                <img :src="`${member.image}`" :alt="`${member.name}`" class="w-100 max-w-50">
+                <div>
+                    <h4 class="text-xl">{{ member.name }}</h4>
+                    <h5 class="text-[14px]">{{ member.title }}</h5>
+                    <p class="text-[14px]">{{ member.profession }}</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script setup>
+
+const team = ref([
+    {
+        image: "/assets/img/alexis.png",
+        name: "Alexis Bourla",
+        title: "CEO",
+        profession: "Psychiatre",
+    },
+    {
+        image: "/assets/img/alexis.png",
+        name: "Jean-François Ramadier",
+        title: "CTO",
+        profession: "Ingénieur en IA",
+    },
+    {
+        image: "/assets/img/alexis.png",
+        name: "Redwan Maatoug",
+        title: "Scientific advisor",
+        profession: "Psychiatre et chercheur en neurosciences",
+    },
+    {
+        image: "/assets/img/alexis.png",
+        name: "Michel Gennaoui",
+        title: "Scientific director",
+        profession: "Nephrologue",
+    },
+])
+
+</script>
--- pages/maquette2.vue
+++ pages/maquette2.vue
@@ -4,8 +4,8 @@
     <div class="flex justify-center text-center text-3xl pb-6">Ils nous accompagnent :</div>
     <div class="flex flex-wrap gap-4 justify-center">
       <div class="basis-full md:basis-[30%] flex bg-center bg-no-repeat bg-contain h-24 grayscale bg-[url('/assets/img/logo-ipeps2.svg')] " />
-      <div class="basis-full md:basis-[30%] flex bg-center bg-no-repeat bg-contain min-h-min h-24 brightness-200 bg-[url('/assets/img/logo-icrin.png')]" />
-      <div class="basis-full md:basis-[30%] flex bg-center bg-no-repeat bg-contain min-h-min h-24 grayscale bg-[url('/assets/img/logo-fondationkorian2.svg')]" />
+      <div class="basis-full md:basis-[30%] flex bg-center bg-no-repeat bg-contain h-24 brightness-200 bg-[url('/assets/img/logo-icrin.png')]" />
+      <div class="basis-full md:basis-[30%] flex bg-center bg-no-repeat bg-contain h-24 grayscale bg-[url('/assets/img/logo-fondationkorian2.svg')]" />
     </div>
   </div>
 
