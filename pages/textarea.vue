13ba5bf67499267784b0c6e0a617f6855d9c0009
Maxime Coriton <maxime.coriton@gmail.com>
2022-11-22 13:10:07 +0100

    html

--- pages/textarea.vue
+++ pages/textarea.vue
@@ -1,19 +1,19 @@
 <template>
-    <textarea ref="element" v-on:change="auto_grow(element)"></textarea>
+  <textarea @focus="auto_grow(this)"></textarea>
 </template>
 
 <script setup>
-const auto_grow  = (element) => {
-    element.style.height = "5px";
-    element.style.height = (element.scrollHeight)+"px";
+function auto_grow(element) {
+  element.style.height = "5px";
+  element.style.height = element.scrollHeight + "px";
 }
 </script>
 
 <style>
 textarea {
-    resize: none;
-    overflow: hidden;
-    min-height: 50px;
-    max-height: 100px;
+  resize: none;
+  overflow: hidden;
+  min-height: 50px;
+  max-height: 100px;
 }
 </style>
